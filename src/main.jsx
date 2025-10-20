3b10dde20299360bcb63c7b98d1d8091ac78bac3
Jayanth G <jayanthgorrepati14@gmail.com>
2025-10-20 11:21:39 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import Parent from './components/Parent.jsx'
+// import Parent from './components/Parent.jsx'
+import Parent from './Parent.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
