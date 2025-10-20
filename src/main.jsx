aa13d6d8b46e48478540d2f006bd9fd59927cfcd
Jayanth G <jayanthgorrepati14@gmail.com>
2025-10-20 12:55:29 +0530

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 // import Parent from './components/Parent.jsx'
+import { HashRouter } from 'react-router-dom'
 import Parent from './Parent.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter basename='/Link-Flow'>
+
+  <HashRouter basename='/Link-Flow'>
       <Parent></Parent>
-    </BrowserRouter>
+  </HashRouter>    
+
 )
